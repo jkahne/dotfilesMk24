ebb2a5abf0cefc359b16813239cca34ba1f8b711
jkahne <jkahne@fastmail.com>
2024-04-20 15:19:34 -0400

    add lazygit to nvim

--- dot-config/nvim/lua/homeless/lazy/fugitive.lua
+++ dot-config/nvim/lua/homeless/lazy/fugitive.lua
@@ -1,8 +1,34 @@
 return {
+  {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "git", ":Git ")
-        vim.keymap.set("n", "gb",  ":Git blame<CR>")
-        vim.keymap.set("n", "gap", ":Git add -p<CR>")
+      vim.keymap.set("n", "git", ":Git ")
+      vim.keymap.set("n", "gb",  ":Git blame<CR>")
+      vim.keymap.set("n", "gap", ":Git add -p<CR>")
     end
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lz", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    },
+
+    config = function()
+      require("lazy").setup()
+    end
+  },
 }
