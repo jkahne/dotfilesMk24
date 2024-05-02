b72f888b5c2fac1b7e39d8895d64630b009103e3
jkahne <jkahne@fastmail.com>
2024-05-01 21:18:25 -0400

    plugins under eval

--- dot-config/nvim/lua/homeless/lazy/init.lua
+++ dot-config/nvim/lua/homeless/lazy/init.lua
@@ -10,6 +10,39 @@ return {
       require('nvim-web-devicons').setup({})
     end
   },
+    -- filename
+  -- {
+  --   "b0o/incline.nvim",
+  --   dependencies = {},
+  --   event = "BufReadPre",
+  --   priority = 1200,
+  --   config = function()
+  --     local helpers = require("incline.helpers")
+  --     require("incline").setup({
+  --       window = {
+  --         padding = 0,
+  --         margin = { horizontal = 0 },
+  --       },
+  --       render = function(props)
+  --         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+  --         local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
+  --         local modified = vim.bo[props.buf].modified
+  --         local buffer = {
+  --           ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) }
+  --           or "",
+  --           " ",
+  --           { filename, gui = modified and "bold,italic" or "bold" },
+  --           " ",
+  --           -- guibg = "#6699aa",
+  --           guibg = "#FFFC1F",
+  --           guifg = "#000000",
+  --         }
+  --         return buffer
+  --       end,
+  --     })
+  --   end,
+  -- },
+
   { 'tpope/vim-abolish' },
   {
     "preservim/vim-pencil",
@@ -91,6 +124,8 @@ return {
       vim.keymap.set('n', '<leader>aa', '<cmd>AerialToggle!<CR>')
     end
   },
+  -- consider using https://github.com/klen/nvim-test instead
+  -- looks like you can configure to append docker commands to the test commands (see comments below 🤝)
   {
     "vim-test/vim-test",
     config = function()
@@ -102,6 +137,35 @@ return {
 
     end
   },
+  -- comment 🤝 here 
+  -- This is code taken from https://gitlab.com/oinak/dot_config_nvim/-/blob/main/lua/plugins/languages.lua?ref_type=heads
+  -- I have not tried this yet
+  --  {
+  --   'klen/nvim-test', -- run tests from nvim
+  --   dependencies = { "nvim-treesitter/nvim-treesitter", },
+  --   config = function()
+  --     require('nvim-test').setup({
+  --       termOpts = {
+  --         direction = "horizontal", -- terminal's direction ("horizontal"|"vertical"|"float")
+  --       },
+  --     })
+  --
+  --     require('nvim-test.runners.rspec'):setup({
+  --       command = 'docker-compose',
+  --       args = { "exec", "web", "bundle", "exec", "rspec" },
+  --       file_pattern = "\\v(spec_[^.]+|[^.]+_spec)\\.rb$",
+  --       find_files = { "{name}_spec.rb" },
+  --     })
+  --
+  --     vim.keymap.set("n", "<leader>tn", vim.cmd.TestNearest, { desc = '[T]est [N]earest' })
+  --     vim.keymap.set("n", "<leader>tf", vim.cmd.TestFile, { desc = '[T]est [F]ile' })
+  --     vim.keymap.set("n", "<leader>ts", vim.cmd.TestSuite, { desc = '[T]est [S]uite' })
+  --     vim.keymap.set("n", "<leader>tl", vim.cmd.TestLast, { desc = '[T]est [L]ast' })
+  --     vim.keymap.set("n", "<leader>tv", vim.cmd.TestVisit, { desc = '[T]est [V]isit' })
+  --   end,
+  -- },
+
+
   {
     "elixir-tools/elixir-tools.nvim",
     version = "*",
