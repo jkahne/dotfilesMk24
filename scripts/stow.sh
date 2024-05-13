5c2efcea5f7b0e962bf03655b644538aaad2f9f0
jkahne <jkahne@fastmail.com>
2024-05-12 21:46:34 -0400

    wip

---
+++ scripts/stow.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOTFILES_DIR="$(dirname "$SCRIPT_DIR")"
+
+
+pushd "$(dirname "$SCRIPT_DIR")"
+
+echo "[+] Stowing dotfiles..."
+stow . -t $HOME -v --dotfiles \
+  --ignore=.DS_Store \
+  --ignore=README.md \
+  --ignore=aliases.zsh \
+  --ignore=font \
+  --ignore=key-bindings.zsh \
+  --ignore=prompt.zsh  \
+  --ignore=scripts \
+  --ignore=synced 
+
+popd 
+
+
+
