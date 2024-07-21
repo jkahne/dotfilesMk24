59b164fa0d964bac51b940cd9eabb7a246105351
jkahne <jkahne@fastmail.com>
2024-07-21 19:03:39 -0400

    comment out a bunch of stuff

--- dot-config/nvim/lua/homeless/lazy/ai.lua
+++ dot-config/nvim/lua/homeless/lazy/ai.lua
@@ -1,22 +1,22 @@
 return {
   -- { 'github/copilot.vim' },
-  {
-    "zbirenbaum/copilot-cmp",
-    event = "InsertEnter",
-    dependencies = { "zbirenbaum/copilot.lua" },
-    config = function()
-      vim.defer_fn(function()
-        -- require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-
-        require("copilot").setup({
-          suggestion = { enabled = false },
-          panel = { enabled = false },
-        })
-
-        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-      end, 100)
-    end,
-  },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   event = "InsertEnter",
+  --   dependencies = { "zbirenbaum/copilot.lua" },
+  --   config = function()
+  --     vim.defer_fn(function()
+  --       -- require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+  --
+  --       require("copilot").setup({
+  --         suggestion = { enabled = false },
+  --         panel = { enabled = false },
+  --       })
+  --
+  --       require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+  --     end, 100)
+  --   end,
+  -- },
   --
   -- {
   -- 'robitx/gp.nvim',
