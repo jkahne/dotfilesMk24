7c09765cb52f9d0459c4590b3a34f86f4cb2de66
jkahne <jkahne@fastmail.com>
2024-09-02 14:45:47 -0400

    remove copilot from init since it's been extracted

--- dot-config/nvim/lua/homeless/lazy/init.lua
+++ dot-config/nvim/lua/homeless/lazy/init.lua
@@ -1,21 +1,4 @@
 return {
-	{
-		"zbirenbaum/copilot-cmp",
-		event = "InsertEnter",
-		dependencies = { "zbirenbaum/copilot.lua" },
-		config = function()
-			vim.defer_fn(function()
-				-- require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-
-				require("copilot").setup({
-					suggestion = { enabled = false },
-					panel = { enabled = false },
-				})
-
-				require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-			end, 100)
-		end,
-	},
 
 	{
 		"stevearc/oil.nvim",
