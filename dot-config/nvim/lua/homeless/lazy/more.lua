94feb5353b99437d5e22aeee57ba0aac385bf060
jkahne <jkahne@fastmail.com>
2024-09-02 14:18:36 -0400

    tailwind tools

--- dot-config/nvim/lua/homeless/lazy/more.lua
+++ dot-config/nvim/lua/homeless/lazy/more.lua
@@ -335,6 +335,48 @@ return {
 		end,
 	},
 	{
+		"luckasRanarison/tailwind-tools.nvim",
+		name = "tailwind-tools",
+		build = ":UpdateRemotePlugins",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim", -- optional
+			"neovim/nvim-lspconfig", -- optional
+		},
+		opts = {}, -- your configuration
+		config = function()
+			require("tailwind-tools").setup({
+				document_color = {
+					enabled = false, -- can be toggled by commands
+					kind = "background", -- "inline" | "foreground" | "background"
+				},
+
+				vim.api.nvim_set_keymap(
+					"n",
+					"<leader>fc",
+					"<cmd>:TailwindColorToggle<CR>",
+					{ noremap = true, silent = true }
+				),
+			})
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"luckasRanarison/tailwind-tools.nvim",
+			"onsails/lspkind-nvim",
+		},
+		opts = function()
+			return {
+				formatting = {
+					format = require("lspkind").cmp_format({
+						before = require("tailwind-tools.cmp").lspkind_format,
+					}),
+				},
+			}
+		end,
+	},
+	{
 		"kevinhwang91/nvim-bqf", -- better quickfix
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
