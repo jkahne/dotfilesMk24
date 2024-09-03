e4f8c48b084707e05be06f1dee1faf683706a822
jkahne <jkahne@fastmail.com>
2024-09-03 07:29:36 -0400

    alpha reformat

--- dot-config/nvim/lua/homeless/lazy/alpha.lua
+++ dot-config/nvim/lua/homeless/lazy/alpha.lua
@@ -1,42 +1,41 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
 
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.startify")
-		dashboard.file_icons.provider = "devicons"
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.startify")
+    dashboard.file_icons.provider = "devicons"
 
-		dashboard.section.header.val = {
-			"                                                     ",
-			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-			"                                                     ",
-		}
+    dashboard.section.header.val = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    }
 
-		-- dashboard.section.header.val = {
-		-- 	"                                                                      ",
-		-- 	"  ██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ ██╗  ██╗██╗████████╗",
-		-- 	"  ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗██║ ██╔╝██║╚══██╔══╝",
-		-- 	"  ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝█████╔╝ ██║   ██║   ",
-		-- 	"  ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗██╔═██╗ ██║   ██║   ",
-		-- 	"  ██████╔╝██║  ██║╚██████╗██║  ██╗███████╗██║  ██║██║  ██╗██║   ██║   ",
-		-- 	"  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝   ╚═╝   ",
-		-- 	"                                                                      ",
-		-- }
-		--
+    -- dashboard.section.header.val = {
+    -- 	"                                                                      ",
+    -- 	"  ██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ ██╗  ██╗██╗████████╗",
+    -- 	"  ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗██║ ██╔╝██║╚══██╔══╝",
+    -- 	"  ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝█████╔╝ ██║   ██║   ",
+    -- 	"  ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗██╔═██╗ ██║   ██║   ",
+    -- 	"  ██████╔╝██║  ██║╚██████╗██║  ██╗███████╗██║  ██║██║  ██╗██║   ██║   ",
+    -- 	"  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝   ╚═╝   ",
+    -- 	"                                                                      ",
+    -- }
 
-		alpha.setup(dashboard.opts)
+    alpha.setup(dashboard.opts)
 
-		-- Lua configuration for init.lua
+    -- Lua configuration for init.lua
 
-		-- Set a keymap to show the Alpha dashboard
-		vim.api.nvim_set_keymap("n", "<leader>da", ":Alpha<CR>", { noremap = true, silent = true })
-	end,
+    -- Set a keymap to show the Alpha dashboard
+    vim.api.nvim_set_keymap("n", "<leader>da", ":Alpha<CR>", { noremap = true, silent = true })
+  end,
 }
